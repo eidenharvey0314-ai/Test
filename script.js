331d2404d15457689eb8d75d1b1e1c27dbfa5fde
Website <eidenharvey0314@gmail.com>
2026-07-05 17:34:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,36 @@
 // Face Tracker Main Script
 // =====================================================
 
+const vrButton = document.getElementById("vrButton");
+
+const vrView = document.getElementById("vrView");
+
+const app = document.getElementById("app");
+
+let vrEnabled = false;
+
+vrButton.onclick = () => {
+
+    vrEnabled = !vrEnabled;
+
+    if(vrEnabled){
+
+        app.classList.add("hidden");
+        vrView.classList.remove("hidden");
+
+        vrButton.textContent = "Exit";
+
+    }else{
+
+        app.classList.remove("hidden");
+        vrView.classList.add("hidden");
+
+        vrButton.textContent = "VR";
+
+    }
+
+};
+
 import "./tracker.js";
 
 // Future startup code can go here.
